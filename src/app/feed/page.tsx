4bad187ff2082d575450ce524a6c57acc1c03a34
sedhha <activity.schoolsh2@gmail.com>
2023-07-02 20:01:07 +0530

    Added dummy post mapping

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,4 +1,21 @@
+import dummyPosts from '@/constants/dummy-posts.json';
 const Feed = () => {
-	return <div>Hello Feed</div>;
+	return (
+		<div>
+			Hello Feed
+			<div>
+				{dummyPosts.map((post) => (
+					<div key={post.tripId}>
+						<h1>{post.tripName}</h1>
+						<h2>
+							Starting At: {new Date(post.startDate).toLocaleDateString()}
+						</h2>
+						<h2>Ending At: {new Date(post.endDate).toLocaleDateString()}</h2>
+						<h3>{post.description}</h3>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 export default Feed;
