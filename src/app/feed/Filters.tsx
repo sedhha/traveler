b9717f0c1bd38dc8e9d72c09bd7610230cbea629
sedhha <activity.schoolsh2@gmail.com>
2023-07-03 00:44:51 +0530

    Updated Normal Filters

--- src/app/feed/Filters.tsx
+++ src/app/feed/Filters.tsx
@@ -2,78 +2,104 @@ import classes from './Feed.module.css';
 
 type Props = {
 	apply: boolean;
+	customPrompt: string;
+	startDate: string;
+	endDate: string;
+	location: string;
+	setStartDate: (date: string) => void;
+	setEndDate: (date: string) => void;
 	onUnApply: () => void;
 	onApply: () => void;
+	updateCustomPrompt: (value: string) => void;
+	setLocation: (value: string) => void;
 };
 
-const FilterBox = ({ apply, onUnApply, onApply }: Props) => (
+const FilterBox = ({
+	customPrompt,
+	startDate,
+	endDate,
+	location,
+	setLocation,
+	setStartDate,
+	setEndDate,
+	updateCustomPrompt,
+	onUnApply,
+	onApply,
+}: Props) => (
 	<div className={classes['container']}>
-		<form>
-			<div className={classes['form-group']}>
-				<label
-					className={classes.label}
-					htmlFor='date'
-				>
-					Start Date
-				</label>
-				<input
-					type='date'
-					className={classes.Input}
-					placeholder='Select travel date'
-				/>
-			</div>
-			<div className={classes['form-group']}>
-				<label
-					className={classes.label}
-					htmlFor='date'
-				>
-					End Date
-				</label>
-				<input
-					type='date'
-					className={classes.Input}
-					placeholder='Select travel date'
-				/>
-			</div>
-			<div className={classes['form-group']}>
-				<label
-					className={classes.label}
-					htmlFor='date'
-				>
-					Travel Destination
-				</label>
-				<input
-					type='text'
-					className={classes.Input}
-					placeholder='Start typing location...'
-				/>
-			</div>
-			<div className={classes['form-group']}>
-				<label
-					className={classes.label}
-					htmlFor='prompt'
-				>
-					Summarize your search in not more than 50 characters
-				</label>
-				<textarea
-					id='prompt'
-					className={classes.textarea}
-					placeholder='Enter any specific requirements or preferences'
-				></textarea>
-			</div>
-			<button
-				className={classes.button}
-				type='submit'
+		<div className={classes['form-group']}>
+			<label
+				className={classes.label}
+				htmlFor='date'
 			>
-				Plan My Trip
-			</button>
-		</form>
+				Start Date
+			</label>
+			<input
+				type='date'
+				className={classes.Input}
+				placeholder='Select Start date'
+				onChange={(e) => setStartDate(e.target.value)}
+				value={startDate}
+			/>
+		</div>
+		<div className={classes['form-group']}>
+			<label
+				className={classes.label}
+				htmlFor='date'
+			>
+				End Date
+			</label>
+			<input
+				type='date'
+				className={classes.Input}
+				placeholder='Select End date'
+				onChange={(e) => setEndDate(e.target.value)}
+				value={endDate}
+			/>
+		</div>
+		<div className={classes['form-group']}>
+			<label
+				className={classes.label}
+				htmlFor='date'
+			>
+				Travel Destination
+			</label>
+			<input
+				type='text'
+				className={classes.Input}
+				placeholder='Start typing location...'
+				value={location}
+				onChange={(e) => setLocation(e.target.value)}
+			/>
+		</div>
+		<div className={classes['form-group']}>
+			<label
+				className={classes.label}
+				htmlFor='prompt'
+			>
+				Summarize your search in not more than 50 characters
+			</label>
+			<textarea
+				id='prompt'
+				className={classes.textarea}
+				placeholder='Enter any specific requirements or preferences'
+				value={customPrompt}
+				onChange={(e) => updateCustomPrompt(e.target.value)}
+			></textarea>
+		</div>
+		<button
+			className={classes.button}
+			type='submit'
+			onClick={onApply}
+		>
+			Apply Filters
+		</button>
 		<button
 			id='filterBtn'
 			className={[classes['btn-secondary'], classes.button].join(' ')}
-			onClick={apply ? onUnApply : onApply}
+			onClick={onUnApply}
 		>
-			{apply ? 'Clear Filters' : 'Apply Filters'}
+			Clear Filters
 		</button>
 	</div>
 );
--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -9,51 +9,85 @@ import { TripCard } from './TripCard';
 const Feed = () => {
 	const [showFilter, setShowFilter] = useState(false);
 	const [applied, setApplied] = useState(false);
-	const onApply = () => {
-		setApplied(true);
-		setShowFilter(false);
-	};
-	const onUnApply = () => setApplied(false);
+    const [customPrompt, setCustomPrompt] = useState('');
+		const [startDate, setStartDate] = useState('');
+		const [endDate, setEndDate] = useState('');
+		const [location, setLocation] = useState('');
 
-	const toggleFilter = () => {
-		setShowFilter((prev) => !prev);
-	};
-	return (
-		<>
-			<div className={classes.Filter}>
-				{applied ? (
-					<FaFilter
-						className={classes.FilterIcon}
-						onClick={toggleFilter}
-					/>
-				) : (
-					<FiFilter
-						className={classes.FilterIcon}
-						onClick={toggleFilter}
+		const updateCustomPrompt = (value: string) => setCustomPrompt(value);
+		const onApply = () => {
+			setApplied(true);
+			setShowFilter(false);
+		};
+		const onUnApply = () => setApplied(false);
+
+		const toggleFilter = () => {
+			setShowFilter((prev) => !prev);
+		};
+
+		const filteredPosts = dummyPosts.filter((item) => {
+			const greaterThanEqualToStartDate = !!startDate
+				? new Date(item.startDate) >= new Date(startDate)
+				: true;
+			const lessThanEqualToEndDate = !!endDate
+				? new Date(item.endDate) <= new Date(endDate)
+				: true;
+			const locationMatch = !!location
+				? item.city.toLowerCase().includes(location.toLowerCase()) ||
+				  item.country.toLowerCase().includes(location.toLowerCase())
+				: true;
+			return (
+				(applied &&
+					greaterThanEqualToStartDate &&
+					lessThanEqualToEndDate &&
+					locationMatch) ||
+				!applied
+			);
+		});
+		return (
+			<>
+				<div className={classes.Filter}>
+					{applied ? (
+						<FaFilter
+							className={classes.FilterIcon}
+							onClick={toggleFilter}
+						/>
+					) : (
+						<FiFilter
+							className={classes.FilterIcon}
+							onClick={toggleFilter}
+						/>
+					)}
+				</div>
+				{showFilter && (
+					<FilterBox
+						apply={applied}
+						startDate={startDate}
+						endDate={endDate}
+						customPrompt={customPrompt}
+						location={location}
+						setStartDate={setStartDate}
+						setEndDate={setEndDate}
+						onApply={onApply}
+						onUnApply={onUnApply}
+						updateCustomPrompt={updateCustomPrompt}
+						setLocation={setLocation}
 					/>
 				)}
-			</div>
-			{showFilter && (
-				<FilterBox
-					apply={applied}
-					onApply={onApply}
-					onUnApply={onUnApply}
-				/>
-			)}
-			<div className={classes.TravelPosts}>
-				{dummyPosts.map((post) => (
-					<TripCard
-						key={post.tripId}
-						title={post.tripName}
-						startDate={post.startDate}
-						endDate={post.endDate}
-						description={post.description}
-						city={post.city}
-						country={post.country}
-					/>
-				))}
-			</div>
-		</>
-	);
+				<div className={classes.TravelPosts}>
+					{filteredPosts.map((post) => (
+						<TripCard
+							key={post.tripId}
+							title={post.tripName}
+							startDate={post.startDate}
+							endDate={post.endDate}
+							description={post.description}
+							city={post.city}
+							country={post.country}
+						/>
+					))}
+				</div>
+			</>
+		);
 };
 export default Feed;
